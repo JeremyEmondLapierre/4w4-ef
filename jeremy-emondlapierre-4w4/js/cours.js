18c03cb7e9b8bc0abe23100c9f7d665a288a2636
JeremyEmondLapierre <emondlapierrejeremy@gmail.com>
2021-05-18 11:12:05 -0400

    Fin de l'étape 6 avec la l'adaptadibilité de l'affichage des cours en mode mobile

---
+++ jeremy-emondlapierre-4w4/js/cours.js
@@ -0,0 +1,12 @@
+(function(){
+    //let specifique = document.querySelectorAll('.articleCours Spécifique');
+    //let image = document.querySelectorAll('.articleCours Image 2d/3d');
+    //let web = document.querySelectorAll('.articleCours Web');
+    //let jeu = document.querySelectorAll('.articleCours Jeu');
+
+    specifique.addEventListener('mousedown', function(){
+        
+        
+    })
+			
+}())	
